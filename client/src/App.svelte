5845ff0e37063a54ca3f0df78638cc5580b24c33
robinj <robinj0726@hotmail.com>
2022-08-20 23:20:47 +0800

    use prettier for formatting code

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,24 +1,21 @@
 <script>
-	import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
+  import TopAppBar, { Row, Section, Title } from "@smui/top-app-bar";
 </script>
 
 <main>
-	<TopAppBar
-	variant="static"
-	dense={false}
-	color='secondary'>
-	  <Row>
-		  <div style="margin-top: 1rem;">
-			  <Title>Streaming/VOD Demo</Title>
-		  </div>
-	  </Row>    
+  <TopAppBar variant="static" dense={false} color="secondary">
+    <Row>
+      <div style="margin-top: 1rem;">
+        <Title>Streaming/VOD Demo</Title>
+      </div>
+    </Row>
   </TopAppBar>
 </main>
 
 <style>
-   main {
-      margin: 0;
-      padding: 0;
-      text-align: center;
-    }
+  main {
+    margin: 0;
+    padding: 0;
+    text-align: center;
+  }
 </style>
