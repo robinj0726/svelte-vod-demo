def97c405ae619f23a03dd104a38d54038c616ff
robinj <robinj0726@hotmail.com>
2022-08-20 22:24:27 +0800

    setup svelte client

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = new Koa();
 const serve = require('koa-static');
 const port = process.env.PORT || 3000;
 
-app.use(serve(__dirname + '/../client/dist/'));
+app.use(serve(__dirname + '/../client/public/'));
 
 app.listen(port, () => {
     console.log(`KOA server listening on port ${port}`)
