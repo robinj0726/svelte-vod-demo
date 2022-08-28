653cc0befe714bc89e1efaf84ffd52ff93047dda
robinj730 <88188467+robinj730@users.noreply.github.com>
2022-08-28 23:59:15 +0800

    App component has state

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,13 +1,24 @@
 <script>
-  import List from './lib/List.svelte'
+  import FileList from "./lib/FileList.svelte";
+  import { LoadMediaList } from "./lib/Requests.js";
+  let listPromise = Promise.resolve([]);
+  $: listPromise = LoadMediaList();
 </script>
 
 <header>
   <h1>VOD Demo</h1>
 </header>
 <main>
-  <button class="refresh-btn">Refresh</button>
-  <List />
+  <button class="refresh-btn" on:click={() => {
+    listPromise = LoadMediaList();
+  }}>Refresh</button>
+  {#await listPromise}
+    <!-- Loading… -->
+  {:then list}
+    <FileList files={list} />
+  {:catch err}
+    {err}
+  {/await}
 </main>
 
 <style>
---
+++ client/src/lib/FileList.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let files;
+</script>
+
+<ul>
+    {#each files as el}
+        <div>
+            <a href={"/vod/" + el}>{el}</a>
+        </div>
+    {/each}
+</ul>
+
