e4c7e1909984aac22c0fa7f3ff4dd22049c1712c
robinj730 <88188467+robinj730@users.noreply.github.com>
2022-08-28 21:23:27 +0800

    add koa-router

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
+const router = new Router();
 const app = new Koa();
+
 const serve = require('koa-static');
+app.use(serve(__dirname + '/../client/dist/'));
+
 const port = process.env.PORT || 3000;
 
-app.use(serve(__dirname + '/../client/dist/'));
+router.get('/media/list', ctx => {
+    ctx.body = ["1", "2"];
+});
+
+app.use(router.routes());
 
 app.listen(port, () => {
     console.log(`KOA server listening on port ${port}`)
