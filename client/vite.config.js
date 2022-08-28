e03b798bab18337b6cf4642c2967899ab55a2f41
robinj730 <88188467+robinj730@users.noreply.github.com>
2022-08-28 23:31:14 +0800

    for debug

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/media': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
   plugins: [svelte()]
 })
